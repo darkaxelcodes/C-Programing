6ce865e4887f7e77e6a7fbce66ec4ac82e3c9a79
Satyam Srivastava <srivastavasatyam94@gmail.com>
2020-06-23 20:01:19 +0530

    Create 2.c
    Question : Print the integer '21' using decimal, octal, hexadecimal format.

---
+++ 2.c
@@ -0,0 +1,11 @@
+/*
+Question : Print the integer '21' using decimal, octal, hexadecimal format.
+*/
+#include <stdio.h>
+int main()
+{
+	printf("Printing 21 using hexadecimal notaion 0x15: %d \n", 0x15);
+  printf("Printing 21 using octal notaion 025: %d \n", 0x15);
+  printf("Printing 21 using decimal notaion 21: %d \n", 21);
+	return 0;
+}
